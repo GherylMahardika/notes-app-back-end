710493c526b72b61ef52cd33306967e54b41bfea
Gheryl Mahardika <gherylmahardika15@gmail.com>
2024-11-03 20:48:08 +0700

    fix bugs v.1.1

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,5 @@
 const nanoid = require('nanoid');
 const notes = require('./notes');
-const notes = require('./notes');
 
 function addNoteHandler(request, h){
   const {title, tags, body} = request.payload;
